f47c5c91cb2b3b781ce358fa1b84c06b9a3fe5d6
mohammadnoor <mohammadnoorcse@gmail.com>
2025-08-12 23:18:11 +0600

    updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/profile.png';
+import profileImg from '../../assets/profile.png';
 
 const About = () => {
   return (
@@ -69,7 +69,7 @@ const About = () => {
             gyroscope={true}
           >
             <img
-              src={profileImage}
+              src={profileImg}
               alt="aminur"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
