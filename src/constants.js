e9f47b7255c35f2568948c21969dbe9f93f98514
mohammadnoor <mohammadnoorcse@gmail.com>
2025-08-12 22:47:40 +0600

    working commit

---
+++ src/constants.js
@@ -0,0 +1,287 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import vueLogo from './assets/tech_logo/vue.png';
+import nuxtLogo from './assets/tech_logo/nuxtjs.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import laravelLogo from './assets/tech_logo/laravel.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import mastersysLogo from './assets/company_logo/mastersys.png';
+import coderobitLogo from './assets/company_logo/coderorbit.png';
+import SoftStarzLogo from './assets/company_logo/SoftStarz.png';
+
+// Education Section Logo's
+import daffodilLogo from './assets/education_logo/daffodil.png';
+import urcLogo from './assets/education_logo/urc.jpg';
+import dewparaLogo from './assets/education_logo/dewpara.jpg';
+
+// Project Section Logo's
+import shongkolpoLogo from './assets/work_logo/songkolpo.PNG';
+import qsacademyLogo from './assets/work_logo/qsacademy.png';
+import rokomiiLogo from './assets/work_logo/rokomii.png';
+import restaurantsnoor from './assets/work_logo/restaurants-noor.png';
+import ellaLogo from './assets/work_logo/ella.png';
+import dokan99Logo from './assets/work_logo/dokan99.png';
+import shopLogo from './assets/work_logo/shop.png';
+import facebookLogo from './assets/work_logo/facebook.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Vue', logo: vueLogo },
+      { name: 'Nuxt Js', logo: nuxtLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Laravel', logo: laravelLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: mastersysLogo,
+      role: "Fullstack Developer",
+      company: "Mastersys",
+      type:"Onsite",
+      date: "May 2025 - Present",
+      desc: "Developed full-stack web applications using Laravel for backend API and server-side logic, coupled with modern frontend technologies like HTML, CSS, JavaScript, and Vue.js or React.js to create responsive and interactive user interfaces. Collaborated with design and backend teams to deliver seamless, efficient, and scalable solutions, integrating RESTful APIs and ensuring smooth data flow between client and server.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Laravel",
+        "Tailwind CSS",
+        "MongoDb",
+        "MySQL",
+        "Redux",
+        "Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: coderobitLogo,
+      role: "Frontend Developer",
+      company: "CoderOrbit",
+      type:"Onsite",
+      date: "Junary 2025 - May 2025",
+      desc: "Played a key role as a Fullstack Engineer by driving both frontend and backend development using a diverse tech stack including HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Partnered with cross-functional teams to build responsive, efficient web applications, ensuring smooth technology integration and an enhanced user experience.",
+      skills: [
+        "ReactJS",
+        "Vue",
+        "Nuxt js",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: SoftStarzLogo,
+      role: "Frontend Developer",
+      company: "SoftStarz",
+      type:"Remote",
+      date: "January 2024 - July 2024",
+      desc: "As a Junior Frontend Developer, I was responsible for designing and implementing responsive and scalable user interfaces using modern web technologies such as HTML, CSS, JavaScript, Bootstrap, and Material UI. I collaborated closely with the design team to transform wireframes and prototypes created in Figma into interactive, user-friendly web pages. My role involved ensuring cross-browser compatibility and optimizing the front-end for performance and responsiveness across different devices. I actively participated in code reviews and contributed to improving the overall quality and maintainability of the codebase while troubleshooting UI issues to enhance the user experience.",
+      skills: [
+        "Laravel",
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: daffodilLogo,
+      school: "Daffodil International University",
+      date: "January 2020 - February 2024",
+      grade: "3.41 out 4 ",
+      desc: "I have completed my Bachelor of Science (B.Sc.) in Computer Science from Daffodil International University, graduating with a CGPA of 3.41 out of 4.0. My studies covered key areas such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I also participated in workshops and technical events that helped strengthen my problem-solving, programming, and teamwork skills, contributing significantly to my professional and technical development",
+      degree: "Bachelor of Science - BSC (Computer Science)",
+    },
+   
+    {
+      id: 1,
+      img: urcLogo,
+      school: "Uttara Residential School & College",
+      date: "July 2017 - July 2019",
+      grade: "4.58 out 5",
+      desc: "I completed my Higher Secondary Certificate (HSC) from Uttara Residential School & College with a GPA of 4.58 out of 5. My coursework focused on Science, with core subjects including Physics, Chemistry, Mathematics, and Computer Science. During this time, I developed a strong academic foundation and enhanced my analytical and problem-solving skills.",
+      degree: "Higher School & Certificate",
+    },
+    {
+      id: 2,
+      img: dewparaLogo,
+      school: "Dewpara Gono High School",
+      date: "January 2012 - February 2017",
+      grade: "5 out 5",
+      desc: "I completed my Secondary School Certificate (SSC) from Dewpara Gono High School with a perfect GPA of 5.00 out of 5. My studies were focused on the Science group, which included subjects such as Physics, Chemistry, Mathematics, and Computer Science. This period laid the groundwork for my interest in technology and strengthened my academic discipline.",
+      degree: "Scondary School & Certificate",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "shongkolpo",
+      description:
+        "Shongkolpo Education is an interactive, responsive web application developed using React, HTML5, and Tailwind CSS. The platform is designed to provide users with a seamless educational experience through intuitive navigation and visually appealing layouts. Leveraging React’s component-based architecture, the app delivers dynamic content updates without page reloads, while Tailwind CSS ensures a modern, mobile-first design approach that adapts flawlessly across devices. The project focuses on accessibility, performance, and clean UI/UX to engage learners effectively.",
+      image: shongkolpoLogo ,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "Tailwind"],
+      github: "https://github.com/Mohammadnoorcse/shongkolpo",
+      webapp: "https://shongkolpo.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "qsacademy",
+      description:
+        "QsAcademy Education is a modern, user-centric educational platform built with React and styled using Tailwind CSS to provide a clean, responsive, and intuitive user interface. The project incorporates advanced frontend techniques such as component reusability, state management with React hooks, and smooth navigation for enhanced user experience. Additional technologies, like React Router for seamless page routing and Axios for API integration, enable dynamic content delivery and real-time data fetching. The application is optimized for performance and accessibility, ensuring an engaging learning environment across all devices.",
+      image: qsacademyLogo,
+      tags: ["React JS", "Tailwind", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Mohammadnoorcse",
+      webapp: "https://qsacademybd.com/",
+    },
+    {
+      id: 2,
+      title: "Rokomari clone",
+      description:
+        "Book Store is a full-stack web application designed to provide a seamless online shopping experience for book lovers. The frontend is built with React JS, delivering a dynamic and responsive user interface using HTML, CSS, and JavaScript for smooth interactions and intuitive navigation. The backend is powered by Laravel, which handles data management, user authentication, and business logic. RESTful APIs connect the frontend and backend, enabling efficient communication for operations such as browsing books, managing user carts, and processing orders. The project emphasizes scalability, performance, and an engaging user experience across all devices.",
+      image: rokomiiLogo,
+      tags: ["React JS","laravel", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Mohammadnoorcse/rokomarii",
+      webapp: "https://ebook-one-nu.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Restaurants-noor",
+      description:
+        "Restaurants-Noor is a responsive web application designed to help users discover, explore, and review restaurants. Built with modern technologies like React.js for a dynamic frontend experience, it features intuitive navigation and visually rich interfaces. The platform allows users to browse restaurant listings, view detailed menus, read reviews, and interact through ratings and comments. Backend services handle data management and user authentication, ensuring secure and efficient operations. The project focuses on delivering seamless performance, mobile responsiveness, and an engaging user experience for food enthusiasts.",
+      image: restaurantsnoor,
+      tags: ["React JS", "tailwind", "Html", "Css"],
+      github: "https://github.com/Mohammadnoorcse",
+      webapp: "https://restaurants-noor.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "ella",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: ellaLogo,
+      tags: ["React","JavaScript", "HTML", "CSS"],
+      github: "https://github.com/Mohammadnoorcse/ella",
+      webapp: "https://ella-sandy-phi.vercel.app/",
+    },
+    {
+      id: 5,
+      title: "Dokan99",
+      description:
+        "At SoftStarz Company, I contributed to developing and maintaining robust web applications using Laravel as the backend framework. My responsibilities included building dynamic and responsive user interfaces with HTML, CSS, and JavaScript to ensure seamless user experiences across devices. I collaborated closely with cross-functional teams to implement new features, optimize performance, and fix bugs, while following best practices in coding standards and version control. Additionally, I integrated APIs and worked on server-side logic to support business requirements, delivering scalable and maintainable solutions.",
+      image: dokan99Logo,
+      tags: ["Laravel","HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Mohammadnoorcse",
+      webapp: "https://99dokan.softstarz.com/",
+    },
+    {
+      id: 6,
+      title: "SHOP.CO",
+      description:
+        "Developed a full-featured e-commerce application with Nuxt.js for the frontend, utilizing Tailwind CSS to build a clean, responsive, and user-friendly design. Implemented state management using Pinia to efficiently handle application data and enhance user interactions. On the backend, used Laravel to build robust APIs that manage product catalogs, user authentication, orders, and payment processing. Integrated the frontend with Laravel APIs to enable seamless communication, ensuring fast and reliable data flow across the application. Focused on delivering a smooth shopping experience with optimized performance and maintainability.",
+      image: shopLogo,
+      tags: ["Nuxt js", "Tailwind","Laravel","pinia"],
+      github: "https://github.com/Mohammadnoorcse",
+      webapp: "https://vue-ecom-forntend-2.vercel.app/",
+    },
+    {
+      id: 7,
+      title: "Facebook clone",
+      description:
+        "Developed a Facebook clone application using React to create a dynamic and interactive user interface. Styled the application with Tailwind CSS, HTML, and CSS to build a responsive, modern, and user-friendly layout. Implemented core social media features such as user posts, likes, comments, and real-time updates, focusing on delivering smooth performance and an engaging user experience. The project demonstrates component-based architecture, state management, and responsive design principles.",
+      image: facebookLogo,
+      tags: ["React", "Tailwind","Html","css"],
+      github: "https://github.com/Mohammadnoorcse/facebook-clone",
+      webapp: "https://facebook-one-theta.vercel.app/",
+    },
+ 
+  ];  
